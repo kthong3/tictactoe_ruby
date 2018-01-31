93b77aec3c9511c01cc9608f2e984e778dd6d90d
kthong3 <kthongsakounh@gmail.com>
2018-01-30 23:16:57 -0600

    replace number with letter for player one

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,18 +1,41 @@
-# create board
-
-def pretty_print
-  board = []
-  split_range = (1..9).to_a.each_slice(3) {|set| board << set }
-
-  board.map! do |set|
-    set.join("").insert(1, "|").insert(3, "|")
+class Game
+  def initialize
+    @board = []
+    set_up(@board)
   end
 
-  board.insert(1, "_____").insert(3, "_____")
+  # create board
+  def set_up(board)
+    split_range = (1..9).to_a.each_slice(3) {|set| board << set }
 
-  board.each do |set|
-    puts set
+    board.map! do |set|
+      set.join("").insert(1, "|").insert(3, "|")
+    end
+
+    board.insert(1, "_____").insert(3, "_____")
+
+    board.each do |set|
+      puts set
+    end
   end
+
+  def check_location(number, letter)
+    @board.each do |set|
+      if set.include?(number)
+        location = set.index(number).to_s
+        replace = set.gsub!(location, letter)
+      else
+        "That spot has been taken, please try again."
+      end
+      puts set
+    end
+  end
+
 end
 
-pretty_print
+game = Game.new
+
+puts "Player one, please enter the number where you'd like to mark"
+number = gets.chomp
+letter = "X"
+game.check_location(number, letter)
