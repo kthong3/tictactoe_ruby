d712ac58d621e8fbd4971266f9eccdc684522aa9
kthong3 <kthongsakounh@gmail.com>
2018-01-30 22:05:56 -0600

    save board in array and print each set in nested array

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,7 +1,13 @@
 # create board
 
 def pretty_print
-  (1..9).to_a.each_slice(3) {|number| p number}
+  board = []
+  split_range = (1..9).to_a.each_slice(3) {|set| board << set }
+
+  board.each do |set|
+    p set
+  end
+    # 1.upto(9) { |i| print i, " | " }
 end
 
 pretty_print
