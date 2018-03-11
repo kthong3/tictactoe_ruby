fc538cabeba8d760b4124ac4a0d295861776e9f6
kthong3 <kthongsakounh@gmail.com>
2018-03-11 14:05:32 -0500

    delete tests since they are now private methods in Game class

--- game.rb
+++ game.rb
@@ -1,21 +1,25 @@
 class Game
+  attr_accessor :board
+
   def initialize
     @board = []
     generate_board
   end
 
-  def generate_board
-    (1..9).each_slice(3) {|i| @board << i}
-    @board.map! {|slice| slice.insert(1, "|").insert(3, "|").join("")}
-  end
-
-  def display_board
-    puts @board
+  def start
+    display_board
   end
 
   private
-  attr_reader :board
+  def generate_board
+    (1..9).each_slice(3) {|i| self.board << i}
+    self.board.map! {|slice| slice.insert(1, "|").insert(3, "|").join("")}
+  end
+
+  def display_board
+    puts self.board
+  end
 end
 
 game = Game.new
-game.display_board
+game.start
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,18 +4,6 @@ describe Game do
   let(:game) { Game.new }
 
   describe 'board structure' do
-    describe '#board' do
-      it 'is an array' do
-        expect(game.send :board).to be_an Array
-      end
 
-      it 'is empty' do
-        expect(game.send :board.length).to eq 0
-      end
-    end
-
-    describe '#generate_board' do
-      it 'creates board with 3 rows'
-    end
   end
 end
