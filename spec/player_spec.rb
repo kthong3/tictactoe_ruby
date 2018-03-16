ecea30d50afb116dbf07bc49ec68d641d9e8711b
kthong3 <kthongsakounh@gmail.com>
2018-03-16 10:38:13 -0500

    update player spec tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,15 +3,15 @@ require_relative "../player"
 describe Player do
   let(:player) { Player.new("Player Three", "K") }
 
-  it 'has a player_number' do
+  it "has a player_number" do
     expect(player.player_number). to eq "Player Three"
   end
 
-  it 'has a letter' do
+  it "has a letter" do
     expect(player.letter).to eq "K"
   end
 
-  it 'has an empty location set' do
+  it "has an empty location set" do
     expect(player.location_set). to eq []
     expect(player.location_set.empty?).to be true
   end
